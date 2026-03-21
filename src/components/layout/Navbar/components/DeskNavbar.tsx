5f0b40089d34b6695a4c71b2bdc9edc1d84a700e
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-03-21 20:38:25 +0530

    Created code for Desktop Navbar

---
+++ src/components/layout/Navbar/components/DeskNavbar.tsx
@@ -0,0 +1,30 @@
+// import { Link, useLocation } from 'react-router-dom'
+import { NAVLINKS_DATA } from '../../../../config/navLinks.data'
+
+const DeskNavbar = () => {
+    const navTitle: string = 'DevSphere';
+    // const location = useLocation();
+
+    return (
+        <nav className="w-full fixed top-0 left-0 z-50 bg-black backdrop-blur-xl border-b border-white/10">
+            <div className="max-w-7xl mx-auto px-8 py-4 flex items-center justify-between">
+
+                {/* Title */}
+                <a href='/' className="text-2xl font-bold tracking-tight cursor-pointer font-mono text-white">
+                    {navTitle}
+                </a>
+
+                {/* Links */}
+                <div className="hidden md:flex items-center gap-10">
+                    {NAVLINKS_DATA.map((link, idx) => (
+                        <a key={idx} href={link.path} className={`relative group text-gray-300 hover:text-yellow-400 font-medium transition hover:underline hover:underline-offset-4 font-mono`}>
+                            {link.label}
+                        </a>
+                    ))}
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default DeskNavbar
