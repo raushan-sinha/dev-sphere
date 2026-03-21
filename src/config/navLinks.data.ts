e1ca9a203fe39862bc004643d0726b570883ec34
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-03-21 20:37:50 +0530

    Stored Nav links data

---
+++ src/config/navLinks.data.ts
@@ -0,0 +1,8 @@
+import type { NavbarLinks } from "../types/navTypes";
+
+export const NAVLINKS_DATA: NavbarLinks[] = [
+    { label: 'Product', path: '/product' },
+    { label: 'Services', path: '/services' },
+    { label: 'About', path: '/about' },
+    { label: 'Contact', path: '/contact' },
+]
