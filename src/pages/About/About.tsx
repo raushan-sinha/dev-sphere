7bb02aa745fbc1e59ace38a37ab743c43330d0eb
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-03-23 20:55:55 +0530

    Created new file

---
+++ src/pages/About/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1 className='mt-30'>About</h1>
+        </div>
+    )
+}
+
+export default About
---
+++ src/pages/Product/Product.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Product = () => {
+    return (
+        <h1 className='mt-30'>Product</h1>
+    )
+}
+
+export default Product
