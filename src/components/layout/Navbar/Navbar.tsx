c990a9449c01b660bf18047aa1e1aa5b66789501
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-03-23 20:32:24 +0530

    Added component for MobileNavbar

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -1,9 +1,10 @@
-import DeskNavbar from "./components/DeskNavbar";
+import { DeskNavbar, MobileNavbar } from "./components";
 
 export default function Navbar() {
     return (
         <>
             <DeskNavbar />
+            <MobileNavbar />
         </>
     );
 }
