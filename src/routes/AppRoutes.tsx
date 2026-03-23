312adbd1c03967c01625e2ff3fbf1601db2305c8
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-03-23 20:57:52 +0530

    Added all components with React router dom

---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "../components/layout/Navbar/Navbar";
+import { MobileNavbar } from "../components/layout/Navbar/components";
+import Product from "../pages/Product/Product";
+import Services from "../pages/Services/Services";
+import About from "../pages/About/About";
+import Contact from "../pages/Contact/Contact";
+
+export default function AppRoutes() {
+    return (
+        <>
+            <BrowserRouter>
+                <Navbar />
+                <MobileNavbar />
+
+                <Routes>
+                    <Route path="/product" element={<Product />} />
+                    <Route path="/services" element={<Services />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+}
