9abf52afcf5a5fbaa0344e5fb42d009d19d67f57
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-03-23 20:58:35 +0530

    Added a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import Navbar from "./components/layout/Navbar/Navbar"
+import AppRoutes from "./routes/AppRoutes"
 
 function App() {
 
     return (
         <>
-            <Navbar />
+            <AppRoutes />
         </>
     )
 }
