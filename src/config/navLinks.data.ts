37ac2e6c071b55d440718a0468deffb0243b6948
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-03-25 12:17:31 +0530

    code updated

--- src/config/navLinks.data.ts
+++ src/config/navLinks.data.ts
@@ -2,7 +2,7 @@ import type { NavbarLinks } from "../types/navTypes.types";
 
 export const NAVLINKS_DATA: NavbarLinks[] = [
     { label: 'Product', path: '/product' },
-    { label: 'Services', path: '/services' },
+    { label: 'Service', path: '/service' },
     { label: 'About', path: '/about' },
     { label: 'Contact', path: '/contact' },
 ]
