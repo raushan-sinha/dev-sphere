cd182f246a0db64c31a4b2fa8f3d513184c2f139
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-03-25 11:24:35 +0530

    Code updated

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "../components/layout/Navbar/Navbar";
 import { MobileNavbar } from "../components/layout/Navbar/components";
+import Home from "../pages/home/Home";
 import Product from "../pages/product/Product";
 import Services from "../pages/services/Services";
 import About from "../pages/about/About";
 import Contact from "../pages/contact/Contact";
-import Home from "../pages/home/Home";
 
 export default function AppRoutes() {
     return (
