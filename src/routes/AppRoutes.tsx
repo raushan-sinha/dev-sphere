48402c7f288057fa393b95954e987bbd05ae58bf
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-03-25 15:40:06 +0530

    'Service' component added

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,6 +4,7 @@ import Home from "../pages/Home/Home";
 import Product from "../pages/Product/Product";
 import About from "../pages/About/About";
 import Contact from "../pages/Contact/Contact";
+import Service from "../pages/Service/Service";
 
 export default function AppRoutes() {
     return (
@@ -14,6 +15,7 @@ export default function AppRoutes() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/product" element={<Product />} />
+                    <Route path="/service" element={<Service />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/contact" element={<Contact />} />
                 </Routes>
