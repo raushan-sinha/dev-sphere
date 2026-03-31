f91436303a620e75848e6b7e2a7c24778737920e
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-03-31 17:19:02 +0530

    code updated with new  styles

--- src/components/layout/navbar/modules/DeskNavbar.tsx
+++ src/components/layout/navbar/modules/DeskNavbar.tsx
@@ -9,15 +9,15 @@ const DeskNavbar = () => {
             className="hidden md:flex fixed top-2 sm:top-3 lg:top-4 inset-x-0 z-50 mx-auto w-[95%] max-w-7xl items-center justify-between px-6 py-3 rounded-xl bg-[#101923] backdrop-blur-xl border border-white/10 shadow-[0_8px_30px_rgba(0,0,0,0.4)]">
             {/* Left - Logo */}
             <div className="flex items-center gap-2">
-                <span className="text-white text-2xl font-mono font-bold tracking-wide cursor-pointer">
+                <a href="/" className="text-gray-300 text-2xl font-sans font-bold tracking-wide cursor-pointer">
                     {title}
-                </span>
+                </a>
             </div>
 
             {/* Center - Nav Links */}
             <div className="flex items-center gap-8">
                 {navLinks.map((link) => (
-                    <a key={link.path} href={link.path} className="text-base font-mono font-semibold text-white hover:text-amber-400 transition cursor-pointer">
+                    <a key={link.path} href={link.path} className="text-sm font-sans font-semibold text-gray-300 hover:text-amber-400 transition-colors duration-200 cursor-pointer">
                         {link.name}
                     </a>
                 ))}
