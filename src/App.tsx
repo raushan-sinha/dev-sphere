1fab850804c5efb44db53f349b66c1def3cb22d4
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-04-02 22:51:49 +0530

    Added - React router dom (BrowserRouter, Route, Routes) and Pages Route.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/navbar/Navbar";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Blog from "./pages/blog/Blog";
+import Projects from "./pages/projects/Projects";
+import Analytics from "./pages/analytics/Analytics";
 
 const App = () => {
     return (
-        <Navbar />
+        <BrowserRouter>
+            <Navbar />
+
+            <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/blog" element={<Blog />} />
+                <Route path="/analytics" element={<Analytics />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
