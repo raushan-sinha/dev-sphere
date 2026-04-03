3d8c75eb958b7b8bb4507a4ea240c7fbf1b1956a
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-04-03 22:04:19 +0530

    Added logic for Menu open & close

--- src/components/layout/navbar/modules/MobileNavbar.tsx
+++ src/components/layout/navbar/modules/MobileNavbar.tsx
@@ -1,39 +1,48 @@
-import { Menu, Bedtime } from "@mui/icons-material";
+import { Menu, Bedtime, Clear } from "@mui/icons-material";
+import { useState } from "react";
 
 const title: string = "DevSphereOS";
 
 const MobileNavbar = () => {
-    return (
-        <nav
-            role="navigation"
-            aria-label="Mobile Navigation"
-            className="md:hidden fixed top-2 inset-x-0 z-50 mx-auto w-[95%] flex items-center justify-between px-4 py-3 rounded-xl bg-[#101923] backdrop-blur-md border border-white/10 shadow-[0_8px_30px_rgba(0,0,0,0.4)]">
-            {/* Left - Title */}
-            <a href="/"
-                className="text-gray-300 text-lg font-sans font-bold tracking-wide gradient-text focus:outline-none">
-                {title}
-            </a>
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
-            {/* Right - Controls */}
-            <div className="flex items-center gap-3">
+  return (
+    <nav
+      role="navigation"
+      aria-label="Mobile Navigation"
+      className="md:hidden fixed top-2 inset-x-0 z-50 mx-auto w-[95%] flex items-center justify-between px-4 py-3 rounded-xl bg-[#101923] backdrop-blur-md border border-white/10 shadow-[0_8px_30px_rgba(0,0,0,0.4)]">
+      {/* Left - Title */}
+      <a
+        href="/"
+        className="text-gray-300 text-lg font-sans font-bold tracking-wide gradient-text focus:outline-none">
+        {title}
+      </a>
 
-                {/* Menu Icon */}
-                <button className="w-9 h-9 flex items-center justify-center rounded-lg bg-white/5 border border-white/10 hover:bg-white/10 transition">
-                    <Menu className="text-white" />
-                </button>
+      {/* Right - Controls */}
+      <div className="flex items-center gap-3">
+        {/* Menu Icon */}
+        <button
+          className="w-9 h-9 flex items-center justify-center rounded-lg bg-white/5 border border-white/10 hover:bg-white/10 transition"
+          onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? (
+            <Clear className="text-white" />
+          ) : (
+            <Menu className="text-white" />
+          )}
+        </button>
 
-                {/* Theme Icon */}
-                <div className="w-9 h-9 flex items-center justify-center rounded-lg bg-white/5 border border-white/10">
-                    <Bedtime className="text-white" />
-                </div>
+        {/* Theme Icon */}
+        <div className="w-9 h-9 flex items-center justify-center rounded-lg bg-white/5 border border-white/10">
+          <Bedtime className="text-white" />
+        </div>
 
-                {/* Profile */}
-                <div className="w-8 h-8 flex items-center justify-center rounded-full bg-blue-400 text-white text-sm font-semibold">
-                    R
-                </div>
-            </div>
-        </nav>
-    );
+        {/* Profile */}
+        <div className="w-8 h-8 flex items-center justify-center rounded-full bg-blue-400 text-white text-sm font-semibold">
+          R
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default MobileNavbar;
