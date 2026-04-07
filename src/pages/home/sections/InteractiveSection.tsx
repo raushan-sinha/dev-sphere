10ffe307c6a666575ca64deb71aa9dd6a4904918
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-04-07 21:10:12 +0530

    Added Logics & UI interfaces

---
+++ src/pages/home/sections/InteractiveSection.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { statsData } from '../../../config/statsData.data'
+
+const InteractiveSection: React.FC = () => {
+    return (
+        <section className="px-6 py-20 bg-black/40">
+            <div className="max-w-6xl mx-auto grid md:grid-cols-4 gap-8 text-center">
+                {statsData.map((item, idx) => (
+                    <div key={idx}>
+                        <h4 className="text-3xl font-bold text-blue-500">{item.value}</h4>
+                        <p className="text-gray-400 mt-2">{item.label}</p>
+                    </div>
+                ))}
+            </div>
+        </section>
+    )
+}
+
+export default InteractiveSection
