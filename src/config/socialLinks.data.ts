bda548f446e0841b4d20353cd15a23cf5495f13a
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-04-07 15:50:05 +0530

    Added socil links da

---
+++ src/config/socialLinks.data.ts
@@ -0,0 +1,21 @@
+import { GitHub, Reddit, Code } from "@mui/icons-material";
+import type { ProfileBoardSocialIcons } from "../types/profileBoard.types";
+
+//todo: Social links -
+export const socialLinks: ProfileBoardSocialIcons[] = [
+    {
+        icon: GitHub,
+        name: "GitHub",
+        url: "https://github.com/raushan-sinha"
+    },
+    {
+        icon: Reddit,
+        name: "Reddit",
+        url: "https://www.reddit.com/user/raushan_sinha2004/"
+    },
+    {
+        icon: Code,
+        name: "Dev.to",
+        url: "https://dev.to/raushan_sinha_8efb05c7b1c"
+    }
+]
