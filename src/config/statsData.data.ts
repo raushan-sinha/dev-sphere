602112daad859db01fe6d331c1524b5781f5d8f5
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-04-07 21:09:42 +0530

    Added Data for - InteractiveSection

---
+++ src/config/statsData.data.ts
@@ -0,0 +1,8 @@
+import type { statsType } from "../types/stats.types";
+
+export const statsData: statsType[] = [
+    { value: "10K+", label: "Developers Onboarded" },
+    { value: "500+", label: "Projects Built" },
+    { value: "99.9%", label: "Uptime" },
+    { value: "4.9/5", label: "User Satisfaction" },
+];
