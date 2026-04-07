1003266b21e31c475add3778c2c6e92f74a6a88e
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-04-07 21:10:52 +0530

    Added clean code structure for - InteractiveSection

--- src/pages/home/sections/index.ts
+++ src/pages/home/sections/index.ts
@@ -1,2 +1,3 @@
 export { default as HeroSection } from './HeroSection';
-export { default as featureSection } from './FeaturesSection';
+export { default as FeatureSection } from './FeaturesSection';
+export { default as InteractiveSection } from './InteractiveSection';
