7e83d24a7ff3bced06c69e9da5b90d1af84fffe3
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-04-07 15:53:21 +0530

    File name deleted

--- src/config/navLinksData.ts
+++
@@ -1,8 +0,0 @@
-import type { NAVBARLINKSTYPE } from "../types/navbarLinks.types";
-
-export const navLinks: NAVBARLINKSTYPE[] = [
-    { name: "Home", path: "/" },
-    { name: "Projects", path: "/projects" },
-    { name: "Blog", path: "/blog" },
-    { name: "Analytics", path: "/analytics" },
-];
