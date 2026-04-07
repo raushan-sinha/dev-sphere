641b385fd0fcd454480763042f39e6096140df0e
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-04-07 21:21:11 +0530

    Added type safety for - FooterSection

--- src/pages/home/sections/index.ts
+++ src/pages/home/sections/index.ts
@@ -2,3 +2,4 @@ export { default as HeroSection } from './HeroSection';
 export { default as FeatureSection } from './FeaturesSection';
 export { default as InteractiveSection } from './InteractiveSection';
 export { default as CTASection } from './CTASection';
+export { default as FooterSection } from './FooterSection';
