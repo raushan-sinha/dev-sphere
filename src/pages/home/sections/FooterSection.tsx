a8ee354c727b016840cc6bc3bec9e717d0f52155
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-04-07 21:21:35 +0530

    Added Code

---
+++ src/pages/home/sections/FooterSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FooterSection: React.FC = () => {
+    return (
+        <footer className="border-t border-gray-800 px-6 py-8 text-center text-gray-500 text-sm">
+            © {new Date().getFullYear()} DevSphereOS. All rights reserved.
+        </footer>
+    )
+}
+
+export default FooterSection;
