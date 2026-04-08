2ec3b255bd8324c66a85600d04c399421f7dc909
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-04-08 11:35:31 +0530

    Removed Navbar component & Added new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "./components/layout/navbar/Navbar";
 import Home from "./pages/home/Home";
 import Blog from "./pages/blog/Blog";
 import Projects from "./pages/projects/Projects";
 import Analytics from "./pages/analytics/Analytics";
+import MainLayout from "./components/layout/ui/MainLayout";
 
 const App = () => {
     return (
         <BrowserRouter>
-            <Navbar />
-
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/blog" element={<Blog />} />
-                <Route path="/analytics" element={<Analytics />} />
+                <Route element={<MainLayout />}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="/blog" element={<Blog />} />
+                    <Route path="/analytics" element={<Analytics />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
