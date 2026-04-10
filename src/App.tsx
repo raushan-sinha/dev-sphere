31fa9203dd96d0a3791dcaeef823f660a15910c2
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-04-10 21:20:49 +0530

    Added new components with 'lazy & Suspense' logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/home/Home";
-import Blog from "./pages/blog/Blog";
-import Projects from "./pages/projects/Projects";
-import Analytics from "./pages/analytics/Analytics";
-import MainLayout from "./components/layout/ui/MainLayout";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
 
 const App = () => {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route element={<MainLayout />}>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/projects" element={<Projects />} />
-                    <Route path="/blog" element={<Blog />} />
-                    <Route path="/analytics" element={<Analytics />} />
-                </Route>
-            </Routes>
+            <AppRoutes />
         </BrowserRouter>
     );
 }
