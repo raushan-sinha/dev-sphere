d2ede4f59432f2dc1b7de1738fada89da73a32f3
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-04-14 20:40:21 +0530

    Added types

---
+++ src/types/engineerData.types.ts
@@ -0,0 +1,9 @@
+import type { SvgIconComponent } from "@mui/icons-material";
+
+export interface ENGINEER_DATA {
+    name: string;
+    title: string;
+    image: string;
+    github: string;
+    linkedin: string;
+}
