7910429c926b6b145f6c47eace59be62daae5072
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-04-14 20:40:59 +0530

    Added Data

---
+++ src/config/engineerData.ts
@@ -0,0 +1,25 @@
+import type { ENGINEER_DATA } from "../types/engineerData.types";
+
+export const engineersData: ENGINEER_DATA[] = [
+    {
+        name: "Raushan Sinha",
+        title: "Frontend Engineer",
+        image: "https://i.pravatar.cc/150?img=1",
+        github: "#",
+        linkedin: "#",
+    },
+    {
+        name: "Aman Verma",
+        title: "Full Stack Engineer",
+        image: "https://i.pravatar.cc/150?img=2",
+        github: "#",
+        linkedin: "#",
+    },
+    {
+        name: "Sneha Gupta",
+        title: "DevOps & Cloud Engineer",
+        image: "https://i.pravatar.cc/150?img=3",
+        github: "#",
+        linkedin: "#",
+    },
+];
