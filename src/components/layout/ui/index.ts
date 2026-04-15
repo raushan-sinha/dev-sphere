906c2d80cf9c19cc3249d97de7e3418319a36236
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-04-15 22:04:19 +0530

    Added a code for - ScrollView

--- src/components/layout/ui/index.ts
+++ src/components/layout/ui/index.ts
@@ -1,3 +1,4 @@
 export { default as MainLayout } from './MainLayout';
 export { default as PageLoader } from './PageLoader';
 export { default as ProfileBoard } from './ProfileBoard';
+export { default as ScrollView } from './ScrollView';
