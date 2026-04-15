9089f7c998228f222e55399ecf203ab76f042f3c
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-04-15 14:05:36 +0530

    Removed a dev data & added images link

--- src/config/engineerData.ts
+++ src/config/engineerData.ts
@@ -1,25 +1,20 @@
 import type { ENGINEER_DATA } from "../types/engineerData.types";
+import dev_1 from '../assets/Images/dev_1.jpg'
+import dev_2 from '../assets/Images/dev_2.jpg'
 
 export const engineersData: ENGINEER_DATA[] = [
     {
         name: "Raushan Sinha",
         title: "Frontend Engineer",
-        image: "https://i.pravatar.cc/150?img=1",
+        image: dev_1,
         github: "https://github.com/raushan-sinha",
         linkedin: "https://www.linkedin.com/in/raushan-sinha-4b94452a1/",
     },
     {
         name: "Pavithr Jain",
         title: "Full Stack Engineer",
-        image: "https://i.pravatar.cc/150?img=2",
+        image: dev_2,
         github: "https://github.com/Pavithrj",
         linkedin: "https://www.linkedin.com/in/pavithr-jain/",
-    },
-    {
-        name: "Ahmad Amin",
-        title: "DevOps & Cloud Engineer",
-        image: "https://i.pravatar.cc/150?img=3",
-        github: "#",
-        linkedin: "https://www.linkedin.com/in/ahmad-amin167471/",
     },
 ];
